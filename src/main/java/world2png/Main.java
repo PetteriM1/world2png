7e7d915cbe73ddc8d6252bfcc083b08057062320
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-12-21 22:53:04 +0200

    Update

--- src/main/java/world2png/Main.java
+++ src/main/java/world2png/Main.java
@@ -1,5 +1,6 @@
 package world2png;
 
+import cn.nukkit.level.Level;
 import cn.nukkit.level.Position;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.scheduler.AsyncTask;
@@ -12,34 +13,40 @@ import java.io.IOException;
 
 public class Main extends PluginBase {
 
-    int X;
-    int Y;
+    private int X;
+    private int Y;
+    private String world;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         X = getConfig().getInt("x_size");
         Y = getConfig().getInt("y_size");
-        getServer().getScheduler().scheduleAsyncTask(new AsyncTask() {
+        world = getConfig().getString("world");
+        getServer().getScheduler().scheduleAsyncTask(this, new AsyncTask() {
             @Override
             public void onRun() {
-                getLogger().info("\u00A7aGenerating world.png...");
-                BufferedImage image;
-                image = new BufferedImage(X, Y, BufferedImage.TYPE_INT_RGB);
+                getLogger().info("\u00A7aGenerating picture of world \"" + world + "\"...");
+                BufferedImage image = new BufferedImage(X, Y, BufferedImage.TYPE_INT_RGB);
                 Graphics2D graphics = image.createGraphics();
-                Position spawn = getServer().getDefaultLevel().getSpawnLocation();
+                Level level = getServer().getLevelByName(world);
+                if (level == null) {
+                    getLogger().error("\u00A7cWorld cannot be null!");
+                    return;
+                }
+                Position spawn = level.getSpawnLocation();
                 for (int x = 0; x != X; x++) {
                     for (int y = 0; y != Y; y++) {
-                        graphics.setColor(new Color(getServer().getDefaultLevel().getMapColorAt(spawn.getFloorX() - (X / 2) + x, spawn.getFloorZ() - (Y / 2) + y).getRGB()));
+                        graphics.setColor(new Color(level.getMapColorAt(spawn.getFloorX() - (X >> 1) + x, spawn.getFloorZ() - (Y >> 1) + y).getRGB()));
                         graphics.fillRect(x, y, x, y);
                     }
                     getLogger().info("\u00A7a" + (x * 100 / X) + "% done");
                 }
                 try {
-                    File file = new File(getDataFolder() + "/world.png");
+                    File file = new File(getDataFolder() + File.separator + world + ".png");
                     ImageIO.write(image, "png", file);
-                    getLogger().info("\u00A7aworld.png saved!");
-                } catch (IOException e) {}
+                    getLogger().info("\u00A7a" + getDataFolder() + File.separator + world + ".png" + " saved!");
+                } catch (IOException ignored) {}
             }
         });
     }
